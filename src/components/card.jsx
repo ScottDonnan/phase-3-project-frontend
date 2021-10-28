90cb6ee1a0ba55d10f69b19b91faa44eb05f6226
Scott Donnan <scott.donnan@me.com>
2021-10-28 09:52:47 -0500

    refactored card logic to herocard pages

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,26 +1,9 @@
 
 function Card({id, name, img, hero_id, selectedDeck, getDeckCards}) {
     
-    function handleAddCardToDeck(e) {
-        const cardObj = {
-            deck_id: selectedDeck,
-            card_id: id
-        }
-        console.log(cardObj)
-        fetch("http://localhost:9292/card_decks", {
-          method: "POST",
-          headers: {Accept: "application/json",
-                "Content-Type": "application/json"},
-          body: JSON.stringify(cardObj)
-        })
-        .then(r => r.json())
-        .then(data => console.log(data))
-        getDeckCards(selectedDeck)
-      }
-    
     return (
         <div >
-            <img onClick={handleAddCardToDeck} value={id} src={img} alt={name} />
+            <img value={id} src={img} alt={name} />
         </div>
     );
   }
--- src/components/heroCards.jsx
+++ src/components/heroCards.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import {useState, useEffect} from "react"
 import {useParams} from 'react-router-dom'
 
-function HeroCards() {
+function HeroCards({selectedDeck, getDeckCards}) {
 
 
     const [isLoaded, setIsLoaded] =useState(false);
@@ -20,9 +20,26 @@ function HeroCards() {
     
     if (!isLoaded) return <h1>Loading...</h1> 
 
+    function handleAddCardToDeck(heroId) {
+        const cardObj = {
+            deck_id: selectedDeck,
+            card_id: heroId
+        }
+        console.log(cardObj)
+        fetch("http://localhost:9292/card_decks", {
+          method: "POST",
+          headers: {Accept: "application/json",
+                "Content-Type": "application/json"},
+          body: JSON.stringify(cardObj)
+        })
+        .then(r => r.json())
+        .then(data => console.log(data))
+        getDeckCards(selectedDeck)
+      }
+
     return (
         <div className="HeroCards">   
-            {heroCards.map((hero) => <img src={hero.img} alt={hero.name} />)}
+            {heroCards.map((hero) => <img onClick={() => handleAddCardToDeck(hero.id)} key={hero.id} src={hero.img} alt={hero.name} />)}
         </div>
     );
 }
